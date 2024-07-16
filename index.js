bfc075d6b9e00704d177280109ac90b708cab5b6
sandiarbaa <sandiarbaa@gmail.com>
2024-07-17 00:22:51 +0700

    6# install nodemon

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Setup Framework Express
 const express = require("express");
 const app = express();
 
+// URL Terdaftar
 app.get("/", (req, res) => {
-  res.send("This is homepage!");
+  res.send("<h1>This is homepage with h1</h1>");
 });
 
 app.get("/cats", (req, res) => {
@@ -17,7 +19,6 @@ app.get("/about", (req, res) => {
   res.send("This is about page!");
 });
 
-// mengambil nilai dari route parameter
 app.get("/blog/:judul", (req, res) => {
   console.log(req.params);
   const { judul } = req.params;
@@ -25,7 +26,6 @@ app.get("/blog/:judul", (req, res) => {
   console.log(judul);
 });
 
-// mengambil nilai dari route parameter
 app.get("/blog/:category/:judul/:author", (req, res) => {
   const { category, judul, author } = req.params;
   res.send(
@@ -33,9 +33,7 @@ app.get("/blog/:category/:judul/:author", (req, res) => {
   );
 });
 
-// mengambil nilai dari query string
 app.get("/search", (req, res) => {
-  // console.log(req.query); // jadi bisa mendapatkan beberapa properti sekaligus dari object query
   const { q } = req.query;
   if (!q) {
     res.send("<h1>Search keyword not found!</h1>");
@@ -50,6 +48,3 @@ app.get("*", (req, res) => {
 app.listen(8080, () => {
   console.log("Server is running on http://localhost:8080");
 });
-
-// NOTE :
-// jadi mengirimkan data di express js itu bisa dari route parameter nya dan juga dari query string
