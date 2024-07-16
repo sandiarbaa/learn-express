4585eb4bed02c813eb298a085d3b58c1c5855ba0
sandiarbaa <sandiarbaa@gmail.com>
2024-07-16 22:56:14 +0700

    #3 membuat route untuk menentukan response tertentu

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
 
-// ada object request dan response, dan masing-masing memiliki methodnya sendiri
-// seperti res.send() itu bisa menampilkan string, json, html
-app.use((req, res) => {
-  console.log("we got request!");
-  // console.dir(req);
-  // console.dir(res);
-  // res.send("Hello World!");
-  // res.send("<h1>Hello World!</h1>");
-  res.send({ message: "Hello World!" });
-  // res.status(200).send("Hello World!");
+// URL TERDAFTAR
+// kalau user mengakses route "/", akan diberikan response ini (default route)
+app.get("/", (req, res) => {
+  res.send("This is homepage!");
+});
+
+// kalau user mengaskes route "/cats", akan diberikan response ini
+// LALU kalau misal ada request dengan method yang sama, misal ada 2 method GET, maka yang akan di ambil/dipakai/ditampilkan adalah method GET yang pertama
+app.get("/cats", (req, res) => {
+  res.send("This is cats page!");
+});
+
+// ini ga akan bentrok dengan yg di atas walaupun url nya sama, karena dari method nya saja sudah beda
+app.post("/cats", (req, res) => {
+  res.send("This is cats page from post!");
+});
+
+// kalau user mengaskes route "/about", akan diberikan response ini
+app.get("/about", (req, res) => {
+  res.send("This is about page!");
+});
+
+// ini akan memberikan response ketika user mencoba mengakses url yang tidak terdaftar
+// dan ini harus berada di paling bawah ya, jangan coba-coba di letakkan di atas, karena nanti akan mere-place atau meng-overwrite semua route yang ada
+app.get("*", (req, res) => {
+  res.send("Page not found!");
 });
 
 app.listen(8080, () => {
